d4a28cb7e29d371de2a63ec4bd33b89adeb23cf5
mohithgg <166399043+mohithgg@users.noreply.github.com>
2024-05-01 21:17:36 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Brain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "BrainSqueezer"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "BS9"
 )
 
 var ID ids.ID
